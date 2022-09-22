4d4e93f5aacd37f304345a63a9047039d2a7bc6c
jasonsaini <jasonsaini.exe@gmail.com>
2022-09-22 12:41:25 -0400

    Added player movement w/ dash mechanic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private BoxCollider playerCollider;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    [SerializeField] private float dashSpeed = 10;
+
+    private Vector3 _input;
+
+    private void Start()
+    {
+        //Physics.IgnoreCollision(playerCollider, GetComponent<Collider>());
+    }
+
+    private void Update()
+    {
+        GatherInput();
+        Look();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+            _rb.freezeRotation = true;
+            _rb.AddForce(transform.forward * dashSpeed, ForceMode.Impulse);
+        }
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Look()
+    {
+        if (_input == Vector3.zero) return;
+
+        var rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        _rb.MovePosition(transform.position + transform.forward * _input.normalized.magnitude * _speed * Time.deltaTime);
+
+    }
+    
+    
+}
+
+
+public static class Helpers
+{
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
+}
