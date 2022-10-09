da15b28ea8355400d74e4cd21203319b2d94f56e
jasonsaini <jasonsaini.exe@gmail.com>
2022-10-09 01:44:34 -0400

    Added enemy following and player tracking
    Enemies now rotate to face player and stop when within attacking distance

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+    public Animator animator;
     public Transform player;
     public UnityEngine.AI.NavMeshAgent enemy;
+    public Vector3 lastEnemyvelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Look();
+        
+    }
+
+    void FixedUpdate()
+    {
         enemy.SetDestination(player.position);
+        if (enemy.remainingDistance < enemy.stoppingDistance)
+        {
+            // START attacking
+            animator.enabled = false;
+            lastEnemyvelocity = enemy.velocity;
+            enemy.velocity = Vector3.zero;
+        }
+        else
+        {
+            animator.enabled = true;
+            enemy.velocity = lastEnemyvelocity;
+        }
+
+    }
+
+    void Look()
+    {
+        this.transform.LookAt(player);
     }
 }
