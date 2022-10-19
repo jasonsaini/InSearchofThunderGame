4d74bfd8e75f4b2b218986d3f6872a8c463a6bed
Declan Carter <declanpcarter@gmail.com>
2022-10-19 00:40:27 -0400

    Added Transition System to Scenes

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    public GameObject Thor;
+    private Collider ThorCollider;
+    private Collider Blackness;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Blackness = GetComponent<Collider>();
+        ThorCollider = Thor.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
