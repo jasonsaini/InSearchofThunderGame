80c8032f79f8b2ee4060e5685fda78b5a41886de
jasonsaini <jasonsaini.exe@gmail.com>
2022-10-28 02:57:09 -0400

    Began to abstract damage calculations

---
+++ Assets/Scripts/Enemy/DamageController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    public Enemy enemy;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
