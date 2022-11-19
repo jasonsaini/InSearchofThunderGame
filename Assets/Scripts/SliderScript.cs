d3ce13975f111c2b1a152055c7868f957d578af5
Declan Carter <declanpcarter@gmail.com>
2022-11-19 17:22:13 -0500

    Revert "Revert "Merge branch 'declanBackup' of https://github.com/jasonsaini/InSearchofThunderGame into declanBackup""
    This reverts commit e0f5a1425fb708780c7dba590af301a887edb3a6.

---
+++ Assets/Scripts/SliderScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private ParticleSystem _particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider.onValueChanged.AddListener((v) =>
+        {
+            _particles.Play();
+        });
+    }
+}
